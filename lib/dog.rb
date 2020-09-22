4744a00d7e6db3370caa196c1e5bd420f8579921
Anthony Leon <acidknights89@gmail.com>
2020-09-22 20:10:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,20 @@
 class Dog
-  
+ 
   def name=(dogs_name)
-    @name = dogs_name
+    @this_dogs_name = dogs_name
   end
-  
+ 
   def name
-    @name 
+    @this_dogs_name
   end
-  def breed=(breed)
-    @breed = breed
+  def breed=(dog_breed)
+    @this_dogs_breed = dog_breed
   end
-  
   def breed
-    @breed
+    @this_dogs_breed
   end 
 end
+
+fido = Dog.new 
+fido.name = "Fido"
+fido.name
